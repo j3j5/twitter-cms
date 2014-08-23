b28b5364882a843972e7fa8a0133fbf9956d73de
Julio Foulquié <jfoulquie@gmail.com>
2014-08-23 22:39:16 +0200

    Make the new fields fillable on the model.

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,7 +4,7 @@ class Post extends Eloquent {
 
 	public $timestamps = TRUE;
 
-	protected $fillable = array('owner_id', 'author', 'slug', 'title', 'link', 'image');
+	protected $fillable = array('owner_id', 'author', 'slug', 'title', 'link', 'image', 'created_from_prov', 'created_from_msg', 'created_at');
 
 	public function user() {
 		return $this->belongsTo('User', 'owner_id');
